ec0a9f6aeada4899379b00ebc56291f9b4ba231e
Jens JK. Klambauer <Jens Klambauer@GIS2.local>
2019-02-07 15:04:36 +0100

    Änderung Tilegrid-Size

--- src/SachsenWmsDopLayer.js
+++ src/SachsenWmsDopLayer.js
@@ -1,18 +1,32 @@
 import TileLayer from 'ol/layer/Tile';
 import TileWMS from 'ol/source/TileWMS';
+import {getWidth} from 'ol/extent.js';
+import TileGrid from 'ol/tilegrid/TileGrid.js';
 
 export default class SachsenWmsDopLayer extends TileLayer {
 
     constructor() {
+        let startResolution = getWidth([278000.0, 5560000.0, 506000.0, 5730000.0]) / 512;
+        let resolutions = new Array(22);
+        for (var i = 0, ii = resolutions.length; i < ii; ++i) {
+          resolutions[i] = startResolution / Math.pow(2, i);
+        }
+        let tileGrid = new TileGrid({
+          extent: [278000.0, 5560000.0, 506000.0, 5730000.0],
+          resolutions: resolutions,
+          tileSize: [512, 512]
+        });
+
         const src = new TileWMS({
             url: "https://geodienste.sachsen.de/wms_geosn_dop-rgb/guest", 
             params: {
-                LAYERS: "sn_dop_020"   
+                LAYERS: "sn_dop_020",
+                'TILED': true 
             },
-            projection: 'EPSG:25833'                   
+            tileGrid: tileGrid                
         });
 
-        super({ source: src, visible: false });
+        super({ source: src, visible: true });
         this.set("title", "SachenDop");
         this.set("type", "base");
         this.set("name", "SNDOP");
--- src/WadWmsLayer.js
+++ src/WadWmsLayer.js
@@ -1,20 +1,32 @@
 import TileLayer from 'ol/layer/Tile';
 import TileWMS from 'ol/source/TileWMS';
+// import {getWidth} from 'ol/extent.js';
+// import TileGrid from 'ol/tilegrid/TileGrid.js';
 
 export default class WadWmsLayer extends TileLayer {
 
     constructor() {
+        // let startResolution = getWidth([315000.0, 5607000.0, 352000.0, 5648000.0]) / 512;
+        // let resolutions = new Array(22);
+        // for (var i = 0, ii = resolutions.length; i < ii; ++i) {
+        //   resolutions[i] = startResolution / Math.pow(2, i);
+        // }
+        // let tileGrid = new TileGrid({
+        //   extent: [315000.0, 5607000.0, 352000.0, 5648000.0],
+        //   resolutions: resolutions,
+        //   tileSize: [512, 512]
+        // });
+
         const src = new TileWMS({
             url: "https://geodatendienste.de/wad/?",   //"http://192.168.0.247:8085/wad/qgis_mapserv.fcgi.exe",  http://5.175.25.253/wad/? 
             params: {
                 LAYERS: ['SESSION.Kanal\\Topografie', 'SESSION.Kanal\\Kanal'],    //,['WAD']
                 TRANSPARENT: true   
             },
-            projection: 'EPSG:25833',          
-            transition: 0                 
+            // tileGrid: tileGrid          
         });
 
-        super({ source: src, visible: true });
+        super({ source: src, visible: false });
         this.set("title", "WAD Kanal");
         this.set("name", "WAD");
         // this.setOpacity(0.5);
